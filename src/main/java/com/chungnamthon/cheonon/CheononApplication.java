5cd66f3716c0a657cb72d8408f18e0db0f945252
강예린 <yerin7786@gmail.com>
2025-07-15 23:06:58 +0900

    initial commit

---
+++ src/main/java/com/chungnamthon/cheonon/CheononApplication.java
@@ -0,0 +1,13 @@
+package com.chungnamthon.cheonon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheononApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CheononApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/chungnamthon/cheonon/CheononApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chungnamthon.cheonon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CheononApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
