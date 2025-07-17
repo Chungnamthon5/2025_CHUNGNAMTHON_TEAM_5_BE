5efd0d94ce21b05e2eb20e9ec52e64f5529f651d
강예린 <yerin7786@gmail.com>
2025-07-18 00:16:14 +0900

    chore: 임시로 모든 요청 허용 (JWT 구현 전)

--- src/main/java/com/chungnamthon/cheonon/global/config/SecurityConfig.java
+++ src/main/java/com/chungnamthon/cheonon/global/config/SecurityConfig.java
@@ -3,8 +3,10 @@ package com.chungnamthon.cheonon.global.config;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
@@ -15,5 +17,17 @@ public class SecurityConfig {
     public WebSecurityCustomizer webSecurityCustomizer() {
         return webSecurity -> webSecurity.ignoring()
                 .requestMatchers("/api-docs/**", "/v3/api-docs/**", "/swagger-ui/**");
+    }
+
+    // Todo JWT 구현 완료 후 삭제
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth
+                        .anyRequest().permitAll()
+                );
+
+        return http.build();
     }
 }
