0808df0552f74e878fca1082120707b272809b07
강예린 <yerin7786@gmail.com>
2025-07-18 00:00:10 +0900

    docs: Swagger에 JWT SecurityScheme 설정 추가

--- src/main/java/com/chungnamthon/cheonon/global/config/SwaggerConfig.java
+++ src/main/java/com/chungnamthon/cheonon/global/config/SwaggerConfig.java
@@ -1,13 +1,20 @@
 package com.chungnamthon.cheonon.global.config;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-@Configuration
+@SecurityScheme(
+        name = "bearerAuth",
+        type = SecuritySchemeType.HTTP,
+        scheme = "bearer",
+        bearerFormat = "JWT"
+)
 @OpenAPIDefinition(
         info = @Info(
                 title = "2025 Chungnamthon 5team API",
@@ -15,6 +22,7 @@ import org.springframework.context.annotation.Configuration;
                 description = "2025 Chungnamthon 5team API"
         )
 )
+@Configuration
 public class SwaggerConfig {
 
     @Bean
