37959bb4760d023734e916116b096127572c081c
강예린 <yerin7786@gmail.com>
2025-07-22 18:40:54 +0900

    refactor: 기획 변경으로 MeetingUser 관련 클래스 삭제

--- src/main/java/com/chungnamthon/cheonon/meeting/repository/MeetingUserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.chungnamthon.cheonon.meeting.repository;
-
-import com.chungnamthon.cheonon.meeting.domain.MeetingUser;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface MeetingUserRepository extends JpaRepository<MeetingUser, Long> {
-}
