5cb14e075dfb766213dae21b71b64d4239b48528
강예린 <yerin7786@gmail.com>
2025-07-22 22:36:24 +0900

    feat: 모임 리스트 조회 기능 구현

---
+++ src/main/java/com/chungnamthon/cheonon/meeting/dto/response/MeetingListResponse.java
@@ -0,0 +1,14 @@
+package com.chungnamthon.cheonon.meeting.dto.response;
+
+import com.chungnamthon.cheonon.meeting.domain.value.Location;
+import com.chungnamthon.cheonon.meeting.domain.value.Schedule;
+
+public record MeetingListResponse(
+        Long meetingId,
+        String title,
+        String description,
+        Location location,
+        Schedule schedule,
+        String imageUrl
+) {
+}
