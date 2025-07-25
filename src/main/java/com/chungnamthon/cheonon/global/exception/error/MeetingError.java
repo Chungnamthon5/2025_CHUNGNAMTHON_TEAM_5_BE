c83c17dd3c3746ba1fc8ebe310ff4653d394189f
강예린 <yerin7786@gmail.com>
2025-07-25 21:44:53 +0900

    feat: 모임 가입 신청 상태 예외 처리용 에러 코드 추가

--- src/main/java/com/chungnamthon/cheonon/global/exception/error/MeetingError.java
+++ src/main/java/com/chungnamthon/cheonon/global/exception/error/MeetingError.java
@@ -14,7 +14,8 @@ public enum MeetingError implements BaseError {
     INVALID_OPENCHATURL(HttpStatus.BAD_REQUEST, "올바르지 않은 카카오 오픈채팅방 링크 형식입니다."),
     FORBIDDEN_MEETING_UPDATE(HttpStatus.FORBIDDEN, "본인의 모임 게시글만 수정할 수 있습니다."),
     FORBIDDEN_MEETING_DELETE(HttpStatus.FORBIDDEN, "본인의 모임 게시글만 삭제할 수 있습니다."),
-    FORBIDDEN_MEETING_MEMBER_ACCESS(HttpStatus.FORBIDDEN, "본인이 생성한 모임의 멤버 리스트만 조회할 수 있습니다.");
+    FORBIDDEN_MEETING_MEMBER_ACCESS(HttpStatus.FORBIDDEN, "본인이 생성한 모임의 멤버 리스트만 조회할 수 있습니다."),
+    INVALID_JOIN_REQUEST_STATE(HttpStatus.BAD_REQUEST, "이미 참여 중이거나, 신청하지 않은 모임입니다.");
 
     private final HttpStatus httpStatus;
     private final String message;
