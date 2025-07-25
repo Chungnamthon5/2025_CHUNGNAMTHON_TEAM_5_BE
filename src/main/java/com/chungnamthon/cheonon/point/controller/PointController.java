80e6db9c87bfab4a361eafc19c939bd6fd3ef861
강예린 <yerin7786@gmail.com>
2025-07-25 22:08:49 +0900

    feat: 포인트 내역 조회 API Swagger 문서 상속

--- src/main/java/com/chungnamthon/cheonon/point/controller/PointController.java
+++ src/main/java/com/chungnamthon/cheonon/point/controller/PointController.java
@@ -14,7 +14,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/points")
 @RequiredArgsConstructor
-public class PointController {
+public class PointController implements PointControllerSwagger {
 
     private final PointService pointService;
 
