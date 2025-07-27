3f1a0513d6ddb29dda5c981a3b7e35c3b2cdf9d1
seonu <ysw130866@naver.com>
2025-07-28 02:48:31 +0900

    refactor: 위도,경도에 따른 가맹점 조회 컨트롤러 구현

--- src/main/java/com/chungnamthon/cheonon/local_merchant/controller/MerchantController.java
+++ src/main/java/com/chungnamthon/cheonon/local_merchant/controller/MerchantController.java
@@ -8,6 +8,8 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 
 @RestController
 @RequestMapping("/api/merchants")
@@ -26,6 +28,17 @@ public class MerchantController {
     public ResponseDto<MerchantDto> detail(@PathVariable Long id) {
         MerchantDto dto = merchantService.detail(id);
         return ResponseDto.of(dto, "Merchant detail fetched successfully.");
+    }
+
+    @GetMapping("/range")
+    public ResponseDto<List<MerchantDto>> getMerchantsInView(
+            @RequestParam("swLat") Double swLat,
+            @RequestParam("swLng") Double swLng,
+            @RequestParam("neLat") Double neLat,
+            @RequestParam("neLng") Double neLng
+    ) {
+        List<MerchantDto> dto =  merchantService.getMerchantsInBounds(swLat, swLng, neLat, neLng);
+        return ResponseDto.of(dto,"가맹점 조회 성공");
     }
 }
 
