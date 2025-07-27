3de2c3f2056dd92535d8004b27243298bbcfba1c
강예린 <yerin7786@gmail.com>
2025-07-27 13:43:26 +0900

    refactor: 모임 설명 전체 응답

--- src/main/java/com/chungnamthon/cheonon/meeting/dto/response/MeetingPreviewResponse.java
+++ src/main/java/com/chungnamthon/cheonon/meeting/dto/response/MeetingPreviewResponse.java
@@ -10,24 +10,19 @@ public class MeetingPreviewResponse {
 
     private Long id;
     private String title;
+    private String description;
     private String imageUrl;
     private String location; // Enum을 문자열로
-    private String schedule; // Enum을 문자열로
-    private String shortDescription;
+    private String schedule;
 
     public static MeetingPreviewResponse from(Meeting meeting) {
         return MeetingPreviewResponse.builder()
                 .id(meeting.getId())
                 .title(meeting.getTitle())
+                .description(meeting.getDescription())
                 .imageUrl(meeting.getImageUrl())
                 .location(meeting.getLocation().name())
                 .schedule(meeting.getSchedule().name())
-                .shortDescription(truncateDescription(meeting.getDescription()))
                 .build();
-    }
-
-    private static String truncateDescription(String description) {
-        if (description == null) return "";
-        return description.length() > 50 ? description.substring(0, 50) + "..." : description;
     }
 }
